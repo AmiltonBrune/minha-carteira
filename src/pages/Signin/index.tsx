00bfee56d14b3a4feb4522fae4ed085b23aa4bd5
amiltonbrune <amiltonbrune.22@gmail.com>
2021-06-20 19:16:49 -0300

    Criando estilos para a tela de logn

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Logo, Form, FormTitle } from './styles';
+
 const Signin: React.FC = () => {
-  return <h1>Signin</h1>;
+  return (
+    <Container>
+      <Logo>
+        <img src={logoImg} alt='Logo' />
+        <h2>Minha Carteira</h2>
+      </Logo>
+      <Form>
+        <FormTitle>Entrar</FormTitle>
+        <input type='text' />
+        <input type='text' />
+
+        <button type='submit'>Acessar</button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Signin;
