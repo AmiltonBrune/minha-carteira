5d903859a05209b782504dbe1df919685b59c8af
amiltonbrune <amiltonbrune.22@gmail.com>
2021-06-20 20:52:18 -0300

    Adicionando responsive componente HistoryBox

--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -33,6 +33,10 @@ export const Header = styled.header`
     margin-bottom: 20px;
     padding-left: 16px;
   }
+
+  @media (max-width: 1200px) {
+    flex-direction: column;
+  }
 `;
 export const LegendContainer = styled.ul`
   list-style: none;
@@ -46,7 +50,7 @@ export const Legend = styled.li<ILegendProps>`
   align-items: center;
 
   margin-bottom: 7px;
-  margin-left: 7px;
+  margin-left: 16px;
 
   > div {
     background-color: ${(props) => props.color};
@@ -62,5 +66,12 @@ export const Legend = styled.li<ILegendProps>`
 
   > span {
     margin-left: 5px;
+  }
+
+  @media (max-width: 1200px) {
+    > div {
+      width: 30px;
+      height: 30px;
+    }
   }
 `;
