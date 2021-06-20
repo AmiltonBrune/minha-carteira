636e54889885d318a5f59eafc8ee47711a3dd1cd
amiltonbrune <amiltonbrune.22@gmail.com>
2021-06-20 18:30:30 -0300

    Criando interface para toggle e adicionando o hook para ele

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,23 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
-import emojis from '../../utils/emojis';
 import Toggle from '../Toggle';
+import emojis from '../../utils/emojis';
+
+import { useTheme } from '../../hooks/theme';
 
 import { Container, Profile, Welcome, UserName } from './styles';
 
 const MainHeader: React.FC = () => {
+  const { toggleTheme, theme } = useTheme();
+  const [darkTheme, setDarkTheme] = useState(() =>
+    theme.title === 'dark' ? true : false
+  );
+
+  const handleChangeTheme = () => {
+    setDarkTheme(!darkTheme);
+    toggleTheme();
+  };
+
   const emoji = useMemo(() => {
     const indice = Math.floor(Math.random() * emojis.length);
     return emojis[indice];
@@ -13,7 +25,12 @@ const MainHeader: React.FC = () => {
 
   return (
     <Container>
-      <Toggle>Toogle</Toggle>
+      <Toggle
+        labelLeft='Ligth'
+        labelRight='Dark'
+        checked={darkTheme}
+        onChange={handleChangeTheme}
+      />
       <Profile>
         <Welcome>Olá, {emoji}</Welcome>
         <UserName>Amilton Brune</UserName>
