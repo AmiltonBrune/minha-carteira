cff3afefee58d38a12656cf5a1559d82cc2b780f
amiltonbrune <amiltonbrune.22@gmail.com>
2021-06-20 23:21:53 -0300

    finalizando o responsivo

--- src/components/ContentHeader/styles.ts
+++ src/components/ContentHeader/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-interface IContainerProps {
+interface ITitleContainerProps {
   lineColor: string;
 }
 
@@ -10,10 +10,14 @@ export const Container = styled.div`
   display: flex;
   justify-content: space-between;
 
-  margin-bottom: 20px;
+  margin-bottom: 25px;
+
+  @media (max-width: 320px) {
+    flex-direction: column;
+  }
 `;
 
-export const TitleContainer = styled.div<IContainerProps>`
+export const TitleContainer = styled.div<ITitleContainerProps>`
   > h1 {
     color: ${(props) => props.theme.colors.white};
 
@@ -24,8 +28,28 @@ export const TitleContainer = styled.div<IContainerProps>`
       border-bottom: 10px solid ${(props) => props.lineColor};
     }
   }
+
+  @media (max-width: 420px) {
+    > h1 {
+      font-size: 22px;
+
+      &::after {
+        content: '';
+        display: block;
+        width: 55px;
+        border-bottom: 5px solid ${(props) => props.lineColor};
+      }
+    }
+  }
 `;
 
 export const Controllers = styled.div`
   display: flex;
+
+  @media (max-width: 320px) {
+    width: 100%;
+
+    justify-content: space-around;
+    margin-top: 20px;
+  }
 `;
